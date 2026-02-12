004485964f32c565fc62a1229b0722eb66855a9d
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-02-13 00:05:39 +0530

    Initial Spring Boot project

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+/*
+1. first create project using spring initializer
+2. create a new repository in gitub with same project name
+3. In github showing main and in intellij showing default branch
+  in is master so first change the name of local branch master to main in intellij.
+4. git branch -M main
+5. git remote add origin https://github.com/shaileshsapkalgit/Git-Revert-Practice.git
+6. git init
+*
+* */
