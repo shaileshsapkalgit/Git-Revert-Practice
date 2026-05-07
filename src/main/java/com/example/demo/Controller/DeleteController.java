b600475246f590c2d56ac93fea61b87d701bbdd7
Shailesh Sapkal <shaileshsapkal1829@gmail.com>
2026-05-07 12:53:41 +0530

    this is my revrt commit

--- src/main/java/com/example/demo/Controller/DeleteController.java
+++ src/main/java/com/example/demo/Controller/DeleteController.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class OfficeController {
+public class DeleteController {
     @GetMapping("/")
     public String viewHomePage() {
         return "home";
     }
-
+    @GetMapping("/")
+    public String ooooo() {
+        return "home";
+    }
 
 }
--- src/main/java/com/example/demo/Controller/PracticeController.java
+++ src/main/java/com/example/demo/Controller/PracticeController.java
@@ -2,13 +2,10 @@ package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @Controller
-public class OfficeController {
+public class PracticeController {
     @GetMapping("/")
     public String viewHomePage() {
         return "home";
     }
-
-
 }
